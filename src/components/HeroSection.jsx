225bd97ec290b998c5c0327bd7df02854751fefe
dyCodes <yusufdaudu51@gmail.com>
2022-04-22 02:48:38 +0100

    Update Hero section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,26 +1,33 @@
-import React from 'react'
+import { Container } from 'react-bootstrap'
+import HeroImage from '../assets/img/hero-image.svg'
 
 const HeroSection = () => {
     return (
         <div id="hero" className="hero">
-            <div className="container">
-                <div className="p-3">
-                    <h1 className="_heading mb-2">
-                        Hi, I'm Yusuf Daudu
-                        <h3 className="_sub_heading fw-normal">Frontend + PHP Developer</h3>
-                    </h1>
+            <Container>
+                <div className="row flex-lg-row-reverse _flex_center justify-content-lg-between g-5">
+                    <div className="col-10 col-md-6 col-lg-5 hero_image p-lg-4">
+                        <img src={HeroImage} className="img-fluid" alt='Hero' />
+                    </div>
 
-                    <p className="desc_text mb-4 fw-light">
-                        I am a 19-year old web developer with 2+ years experience in frontend
-                        and core PHP development. I have a strong passion for programming and writing clean, well organized
-                        code.
-                    </p>
-                    <div className="_flex cta">
-                        <a href="assets/Resume-Yusuf-Daudu.pdf" download className="btn btn-light me-4">Download CV</a>
-                        <a href="#contact" className="btn btn-outline-light d-none d-md-block">Contact Me</a>
+                    <div className="col-lg-6 mb-5 mb-lg-0 pt-lg-3">
+                        <h1 className="_heading mb-2">
+                            Hi, I'm Yusuf Daudu
+                            <h3 className="_sub_heading fw-normal">Frontend + PHP Developer</h3>
+                        </h1>
+
+                        <p className="desc_text mb-4 fw-light">
+                            I am a 19-year old web developer with 2+ years experience in frontend
+                            and core PHP development. I have a strong passion for programming and writing clean, well organized
+                            code.
+                        </p>
+                        <div className="_flex cta">
+                            <a href="assets/Resume-Yusuf-Daudu.pdf" download className="btn btn-light me-4">Download CV</a>
+                            <a href="#contact" className="btn btn-outline-light d-none d-md-block">Contact Me</a>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </Container>
         </div>
     )
 }
