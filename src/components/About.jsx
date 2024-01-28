19965609ccfd26f26c54311fd1dacddbf2d0110e
dyCodes <yusufdaudu51@gmail.com>
2024-01-28 02:38:17 +0100

    feat: update site content

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,40 +1,45 @@
-import { Container } from 'react-bootstrap'
-import AboutImage from '../assets/img/yusuf-photo.jpg'
+import { Container } from 'react-bootstrap';
+import AboutImage from '../assets/img/yusuf-photo.jpg';
 
 const About = () => {
-    return (
-        <section id="about" className="_about">
-            <Container>
-                <div className="row flex-lg-row mt-1 mb-4 _flex_center justify-content-lg-between g-5" >
-                    <div data-aos="fade-up" className="col-lg-7">
-                        <div className="section_heading mb-3">
-                            <h2 className="_heading h1">About Me</h2>
-                        </div>
-                        <p className="about_me pe-lg-2">
-                            <p>
-                                I'm a Full-stack web developer with a strong passion for programming and solving technical problems. I am currently pursuing a degree in <b>Computer Science</b> at Lagos State University.
-                            </p>
+	return (
+		<section id='about' className='_about'>
+			<Container>
+				<div className='row flex-lg-row mt-1 mb-4 _flex_center justify-content-lg-between g-5'>
+					<div data-aos='fade-up' className='col-lg-7'>
+						<div className='section_heading mb-3'>
+							<h2 className='_heading h1'>About Me</h2>
+						</div>
+						<p className='about_me pe-lg-2'>
+							<p>
+								I'm a Full-stack web developer with a strong passion for programming and solving technical
+								problems. I am currently pursuing a degree in <b>Computer Science</b> at Lagos State
+								University.
+							</p>
 
-                            <p className='mt-3'>
-                                I started my journey as a web developer in <b>2019</b> and since then I have been able to work with different technologies and frameworks. I have a good understanding of the fundamentals of web development and I am always eager to learn new technologies and improve my skills.
-                                <br />
-                                {/* I enjoy both frontend and backend development and I am always looking for opportunities to work on challenging projects. */}
-                            </p>
+							<p className='mt-3'>
+								I started my journey as a web developer in <b>2019</b> and since then I have been able to work
+								with different technologies and frameworks. I have a good understanding of the fundamentals of
+								web development and I am always eager to learn new technologies and improve my skills.
+								<br />
+								{/* I enjoy both frontend and backend development and I am always looking for opportunities to work on challenging projects. */}
+							</p>
 
-                            <p className='mt-3'>
-                                <b>I'm Skilled</b> in HTML, CSS, SCSS, Bootstrap, JavaScript, ReactJS, jQuery, MySQL, PHP, WordPress Development and proficient with tools like Figma, Git and PhotoShop.
-                            </p>
-                        </p>
-                    </div>
-                    <div data-aos="fade-up" className="col-md-8 col-lg-5 px-5 px-md-4">
-                        <div className="about_image">
-                            <img src={AboutImage} className="img-fluid" alt='Yusuf Daudu' />
-                        </div>
-                    </div>
-                </div>
-            </Container>
-        </section>
-    )
-}
+							<p className='mt-3'>
+								<b>I'm Skilled</b> in HTML, CSS, SCSS, Bootstrap, JavaScript, Typescript, ReactJS, Next.js,
+								jQuery, Node.js, MySQL, PHP, WordPress, etc.
+							</p>
+						</p>
+					</div>
+					<div data-aos='fade-up' className='col-md-8 col-lg-5 px-5 px-md-4'>
+						<div className='about_image'>
+							<img src={AboutImage} className='img-fluid' alt='Yusuf Daudu' />
+						</div>
+					</div>
+				</div>
+			</Container>
+		</section>
+	);
+};
 
-export default About
+export default About;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,51 +1,66 @@
-import { Container } from 'react-bootstrap'
-import { Link } from 'react-scroll'
-import HeroImage from '../assets/img/hero.svg'
+import { Container } from 'react-bootstrap';
+import { Link } from 'react-scroll';
+import HeroImage from '../assets/img/hero.svg';
+import { BsGithub, BsLinkedin } from 'react-icons/bs';
 
 const Hero = () => {
-    return (
-        <section id="hero" className="_hero">
-            <Container>
-                <div className="row flex-lg-row-reverse _flex_center justify-content-lg-between g-5">
-                    <div data-aos="fade-left" data-aos-delay="400"
-                        className="hero_image col-10 col-md-6 col-lg-5 p-lg-5 mb-lg-4">
-                        <img src={HeroImage} className="img-fluid" alt='Hero' />
-                    </div>
+	return (
+		<section id='hero' className='_hero'>
+			<Container>
+				<div className='row flex-lg-row-reverse _flex_center justify-content-lg-between g-5'>
+					<div
+						data-aos='fade-left'
+						data-aos-delay='400'
+						className='hero_image col-10 col-md-6 col-lg-5 p-lg-5 mb-lg-4'>
+						<img src={HeroImage} className='img-fluid' alt='Hero' />
+					</div>
 
-                    <div data-aos="fade-right" data-aos-delay="400"
-                        data-aos-offset="100" className="hero_text col-lg-6 mb-5 mb-lg-2 pt-lg-3">
-                        <HeroText />
-                    </div>
-                </div>
-            </Container>
-        </section>
-    )
-}
+					<div
+						data-aos='fade-right'
+						data-aos-delay='400'
+						data-aos-offset='100'
+						className='hero_text col-lg-6 mb-5 mb-lg-2 pt-lg-3'>
+						<HeroText />
+					</div>
+				</div>
+			</Container>
+		</section>
+	);
+};
 
 const HeroText = () => {
-    return (
-        <>
-            <div className="_header">
-                <h1 className="_heading mb-2">
-                    Hi, I'm Yusuf Daudu
-                </h1>
-                <h3 className="_sub_heading fw-normal">Full-Stack Web Developer</h3>
-            </div>
+	return (
+		<>
+			<div className='_header'>
+				<h1 className='_heading mb-2'>Hi, I'm Yusuf Daudu</h1>
+				<h3 className='_sub_heading fw-normal'>Full-Stack Developer</h3>
+			</div>
 
-            <p className="desc_text mb-4 fw-light">
-                I'm a goal-oriented web developer with adequate experience and knowledge in developing responsive, well-designed and functional web applications.
-            </p>
+			<p className='desc_text mb-4 fw-light'>
+				I'm a goal-oriented software developer with adequate experience and knowledge in developing
+				responsive, well-designed and functional web applications.
+			</p>
 
-            <div className="_flex cta">
-                <a href="assets/Resume-Yusuf-Daudu.pdf" download
-                    className="btn btn-light me-4">Download CV
-                </a>
-                <Link to="contact" duration={400} smooth={true}
-                    className="btn btn-outline-light d-none d-md-block">Contact Me
-                </Link>
-            </div>
-        </>
-    )
-}
+			<div className='_flex cta'>
+				<Link to='contact' duration={400} smooth={true} className='btn btn-outline-light'>
+					Contact Me
+				</Link>
 
-export default Hero
+				<div className='social_media'>
+					<a href='https://github.com/dyCodes' className='' target='_blank' rel='noopener noreferrer'>
+						<BsGithub />
+					</a>
+					<a
+						href='https://linkedin.com/in/yusuf-daudu-tech'
+						className=''
+						target='_blank'
+						rel='noopener noreferrer'>
+						<BsLinkedin />
+					</a>
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default Hero;
