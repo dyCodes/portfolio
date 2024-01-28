4bb8a94619fcae4ba69cd83ed1fb02ae221c0d90
dyCodes <yusufdaudu51@gmail.com>
2024-01-28 02:37:28 +0100

    feat: install sass

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
-import { useEffect } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
-import "aos/dist/aos.css";
-import AOS from "aos";
+import { useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './scss/App.scss';
+import 'aos/dist/aos.css';
+import AOS from 'aos';
 
-import Header from "./layouts/Header";
-import Footer from "./layouts/Footer";
-import Hero from "./components/Hero";
-import Portfolio from "./components/Portfolio";
-import About from "./components/About";
+import Header from './layouts/Header';
+import Footer from './layouts/Footer';
+import Hero from './components/Hero';
+import Portfolio from './components/Portfolio';
+import About from './components/About';
 
 const App = () => {
-  useEffect(() => {
-    AOS.init({ duration: 500, once: true, });
-    AOS.refresh();
-  }, []);
+	useEffect(() => {
+		AOS.init({ duration: 500, once: true });
+		AOS.refresh();
+	}, []);
 
-  return (
-    <div className="home">
-      <Header />
-      <main>
-        <Hero />
-        <About />
-        <Portfolio />
-      </main>
-      <Footer />
-    </div>
-  );
+	return (
+		<div className='home'>
+			<Header />
+			<main>
+				<Hero />
+				<About />
+				<Portfolio />
+			</main>
+			<Footer />
+		</div>
+	);
 };
 
 export default App;
